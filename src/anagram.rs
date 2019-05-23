d703139b0966047672950fda72af69c565f79367
Aurorans Solis <primalucegd@gmail.com>
2019-05-22 20:49:26 -0700

    Fixed grammatical error.

--- src/anagram.rs
+++ src/anagram.rs
@@ -11,7 +11,7 @@ command!(Anagram(_context, message) {
     to_scramble.shuffle(&mut rng);
     let end_msg = String::from_utf8(to_scramble.into_iter().map(|c| c as u8).collect::<Vec<u8>>())
         .unwrap();
-    let msg = format!("Hey guys, did you know that \"{}\" is an anagram of \"{}?\"", end_msg,
+    let msg = format!("Hey guys, did you know that \"{}\" is an anagram of \"{}\"?", end_msg,
         start_msg);
     let _ = message.delete();
     let _ = message.channel_id.send_message(|m| m.content(msg.as_str()));
