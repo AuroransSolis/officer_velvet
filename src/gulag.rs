838e25b69d76b8a47ef317e4fdd07b4dcb1704bd
Aurorans Solis <primalucegd@gmail.com>
2020-10-31 17:34:07 -0700

    Changed debug formatting, and made duration calculation better.

--- src/gulag.rs
+++ src/gulag.rs
@@ -17,9 +17,9 @@ use std::{
 use structopt::{clap::AppSettings, StructOpt};
 
 fn parse_user_id_from_mention(s: &str) -> AnyResult<UserId> {
-    println!("    Parsing user ID from '{}'", s);
+    println!("GL | ID | Parsing user ID from '{}'", s);
     let trimmed = s.trim_start_matches("<@!").trim_end_matches(">").trim();
-    println!("    Trimmed to: '{}'", trimmed);
+    println!("GL | ID | Trimmed to: '{}'", trimmed);
     let raw_id = trimmed.parse::<u64>()?;
     Ok(raw_id.into())
 }
@@ -90,8 +90,10 @@ struct GulagApp {
 }
 
 fn try_get_gulag(s: &str) -> AnyResult<(UserId, DateTime<Utc>)> {
+    println!("GL | Parsing gulag command use from '{}'", s);
     let trimmed = s.trim_start_matches("=>gulag").trim();
     let arg_matches = GulagApp::from_iter_safe(trimmed.split_whitespace())?;
+    println!("GL | Successfully parsed usage.");
     let GulagApp {
         user_id,
         end_date,
@@ -102,49 +104,54 @@ fn try_get_gulag(s: &str) -> AnyResult<(UserId, DateTime<Utc>)> {
         duration_weeks,
     } = arg_matches;
     let end = if let Some(end) = end_date {
+        println!("GL | DR | Duration specified by end time.");
         end
     } else {
+        println!("GL | DR | Duration specified by parts.");
         let now = Utc::now();
-        let constructors_and_options: [(fn(i64) -> Duration, Option<i64>); 5] = [
+        let constructors_and_values: [(fn(i64) -> Duration, Option<i64>); 5] = [
             (Duration::seconds, duration_secs),
             (Duration::minutes, duration_mins),
             (Duration::hours, duration_hours),
             (Duration::days, duration_days),
             (Duration::weeks, duration_weeks),
         ];
-        let mut duration = Duration::zero();
-        for &(constructor, value) in constructors_and_options.iter() {
-            duration = duration + constructor(value.unwrap_or(0));
-        }
+        let duration = constructors_and_values
+            .iter()
+            .map(|&(constructor, value)| constructor(value.unwrap_or(0)))
+            .fold(Duration::zero(), |acc, new| acc + new);
+        println!("GL | DR | Summed parts of duration to {}", duration);
+        println!("GL | DR | Adding duration to present time UTC.");
         now.checked_add_signed(duration).ok_or_else(|| {
             let msg = format!(
-                "Duration ({}) causes overflow when used as an offset from present time.",
+                "GL | DR | Duration ({}) causes overflow when used as an offset from present time.",
                 duration
             );
             IoError::new(ErrorKind::InvalidInput, msg.as_str())
         })?
     };
+    println!("GL | Successfully parsed user ID and gulag duration.");
     Ok((user_id, end))
 }
 
 #[command]
 pub async fn gulag(ctx: &Context, message: &Message) -> CommandResult {
-    println!("Start handling gulag command.");
+    println!("CM | Start handling gulag command.");
     let start = Instant::now();
-    println!("    Grabbing read 'lock' on context data.");
+    println!("GL | Grabbing read 'lock' on context data.");
     let context_data = ctx.data.read().await;
     let self_id = *context_data.get::<BotIdKey>().unwrap();
-    println!("    Checking permissions.");
+    println!("GL | Checking permissions.");
     if is_administrator(&ctx.http, context_data, &message).await? {
         match try_get_gulag(message.content.as_str()) {
             Ok((user_id, end)) => {
                 if user_id != self_id {
-                    println!("    Getting write lock on context data.");
+                    println!("GL | Getting write lock on context data.");
                     let mut context_data = ctx.data.write().await;
-                    println!("    Getting tasks list.");
+                    println!("GL | Getting tasks list.");
                     let tasks = context_data.get_mut::<TasksKey>().unwrap();
                     println!(
-                        "    Checking for existing gulag entries for user ID {}",
+                        "GL | Checking for existing gulag entries for user ID {}",
                         user_id
                     );
                     // Check if any gulags exist for this user presently, and if they do, update the
@@ -153,19 +160,19 @@ pub async fn gulag(ctx: &Context, message: &Message) -> CommandResult {
                         TaskType::Gulag(gulag) if gulag.user.1 == user_id => Some(gulag),
                         _ => None,
                     }) {
-                        println!("        Found existing gulag entry - updating.");
+                        println!("GL | Found existing gulag entry - updating.");
                         gulag.end = end;
                     } else {
-                        println!("        No gulag entries for that user exist.");
-                        println!("        Getting guild ID.");
+                        println!("GL | No gulag entries for that user exist.");
+                        println!("GL | Getting guild ID.");
                         let guild_id = context_data.get::<ConfigKey>().unwrap().guild_id;
-                        println!("        Getting member information.");
+                        println!("GL | Getting member information.");
                         let mut member =
                             match ctx.http.get_member(guild_id.into(), user_id.into()).await {
                                 Ok(member) => Ok(member),
                                 Err(err) => {
                                     println!(
-                                        "        Failed to get member information. Notifying user."
+                                        "GL | Failed to get member information. Notifying user."
                                     );
                                     let content = format!(
                                         "Failed to get member information. Error details:\n{}",
@@ -176,17 +183,16 @@ pub async fn gulag(ctx: &Context, message: &Message) -> CommandResult {
                                 }
                             }?;
                         println!(
-                            "        Successfully retrieved member information for '{}' (ID {})",
-                            member.display_name(), member.user.id,
+                            "GL | Successfully retrieved member information for '{}' (ID {})",
+                            member.display_name(),
+                            member.user.id,
                         );
                         let user = (member.display_name().clone().into_owned(), user_id);
-                        println!("        Fetching guild information.");
+                        println!("GL | Fetching guild information.");
                         let guild = match ctx.http.get_guild(guild_id.into()).await {
                             Ok(guild) => Ok(guild),
                             Err(err) => {
-                                println!(
-                                    "        Failed to get guild information. Notifying user."
-                                );
+                                println!("GL | Failed to get guild information. Notifying user.");
                                 let content = format!(
                                     "Failed to fetch guild information to save roles. Details:\n{}",
                                     err
@@ -195,32 +201,26 @@ pub async fn gulag(ctx: &Context, message: &Message) -> CommandResult {
                                 Err(err)
                             }
                         }?;
-                        println!("        Successfully retrieved guild information.");
+                        println!("GL | Successfully retrieved guild information.");
                         let roles_map = guild.roles;
-                        println!("        Mapping user IDs to role names.");
-                        let roles = member.roles
+                        println!("GL | Mapping user IDs to role names.");
+                        let roles = member
+                            .roles
                             .iter()
                             .map(|&role_id| {
-                                (
-                                    roles_map
-                                        .get(&role_id)
-                                        .expect("Roles map missing role")
-                                        .name
-                                        .clone(),
-                                    role_id,
-                                )
+                                (roles_map.get(&role_id).unwrap().name.clone(), role_id)
                             })
                             .collect::<Vec<_>>();
-                        println!("        Creating gulag entry.");
+                        println!("GL | Creating gulag entry.");
                         let gulag = Gulag::new(user, roles, end);
-                        println!("        Getting task sender.");
+                        println!("GL | Getting task sender.");
                         let task_sender = context_data.get_mut::<TaskSenderKey>().unwrap();
-                        println!("        Sending task to main thread.");
+                        println!("GL | Sending task to main thread.");
                         match task_sender.send(TaskType::Gulag(gulag)) {
                             Ok(_) => Ok(()),
                             Err(err) => {
                                 println!(
-                                    "        Failed to send task to main thread. Notifying user."
+                                    "GL | SN | Failed to send task to main thread. Notifying user."
                                 );
                                 let content = format!(
                                     "Failed to send gulag task to task handler. Details:\n{}",
@@ -230,22 +230,22 @@ pub async fn gulag(ctx: &Context, message: &Message) -> CommandResult {
                                 Err(err)
                             }
                         }?;
-                        println!("        Successfully sent task to main thread.");
-                        println!("        Getting gulag role ID.");
+                        println!("GL | SN | Successfully sent task to main thread.");
+                        println!("GL | Getting gulag role ID.");
                         let gulag_id = context_data.get::<ConfigKey>().unwrap().prisoner_role_id;
-                        println!("        Removing user's roles.");
+                        println!("GL | Removing user's roles.");
                         let roles = member.roles.clone();
                         member.remove_roles(&ctx.http, &roles).await?;
-                        println!("        Adding prisoner role.");
+                        println!("GL | Adding prisoner role.");
                         ctx.http
                             .add_member_role(guild_id.into(), user_id.into(), gulag_id.into())
                             .await?;
-                        println!("        Successfully gulagged user.");
+                        println!("GL | Successfully gulagged user.");
                     }
                 }
             }
             Err(err) => {
-                println!("    User input an invalid command. Displaying error message.");
+                println!("GL | User input an invalid command. Displaying error message.");
                 let content = format!("Error parsing command. Details:\n```{}\n```", err);
                 let _ = message
                     .channel_id
@@ -256,6 +256,6 @@ pub async fn gulag(ctx: &Context, message: &Message) -> CommandResult {
     } else {
         insufficient_perms(ctx, message).await?;
     }
-    println!("    Elapsed: {:?}", start.elapsed());
+    println!("GL | Elapsed: {:?}", start.elapsed());
     Ok(())
 }
