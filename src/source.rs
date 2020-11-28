bca4684aa58211512d208a2a85eea1f07411a647
Aurorans Solis <primalucegd@gmail.com>
2020-11-27 18:40:21 -0700

    Added debug output info to source command.

--- src/source.rs
+++ src/source.rs
@@ -3,14 +3,18 @@ use serenity::{
     framework::standard::{macros::command, CommandResult},
     model::channel::Message,
 };
+use std::time::Instant;
 
 #[command]
 pub async fn source(ctx: &Context, message: &Message) -> CommandResult {
+    let start = Instant::now();
+    println!("SC | Responding to source command.");
     let _ = message
         .reply(
             &ctx.http,
             "My source code is available at https://github.com/AuroransSolis/officer_velvet.",
         )
         .await?;
+    println!("SC | Elapsed: {:?}", start.elapsed());
     Ok(())
 }
