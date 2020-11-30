1898e221f5f9f746a483b1e1760f840a470522cf
Aurorans Solis <primalucegd@gmail.com>
2020-11-30 00:46:59 -0700

    Updated to use filenames instead of URLs.

--- src/tasks/task.rs
+++ src/tasks/task.rs
@@ -1,13 +1,17 @@
 use super::message::MessageType;
-use crate::{cache_keys::ConfigKey, FILES_DIR};
+use crate::cache_keys::ConfigKey;
 use anyhow::Result as AnyResult;
 use serde::{Deserialize, Serialize};
 use serenity::{
     http::client::Http,
     model::id::ChannelId,
     prelude::{RwLock, TypeMap},
+    utils::read_image,
 };
-use std::{path::PathBuf, sync::Arc};
+use std::{
+    path::PathBuf,
+    sync::Arc,
+};
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub enum Task {
@@ -18,7 +22,7 @@ pub enum Task {
     },
     UpdateAppearance {
         new_name: String,
-        new_icon_url: String,
+        new_icon_filename: String,
     },
 }
 
@@ -29,16 +33,21 @@ impl Task {
                 send_to,
                 message,
                 upload_file,
-                ..
             } => {
                 let mut create_message = message.build();
                 if message.is_embed() {
-                    let context_data = data.read().await;
-                    let icon_url = context_data.get::<ConfigKey>().unwrap().icon_url.as_str();
+                    let icon_url = http.get_current_user().await?.avatar_url().unwrap();
                     create_message.embed(|embed| embed.footer(|footer| footer.icon_url(icon_url)));
                 }
                 if let Some(filename) = upload_file.as_ref() {
-                    let mut base = PathBuf::from(FILES_DIR);
+                    let files_dir = data
+                        .read()
+                        .await
+                        .get::<ConfigKey>()
+                        .unwrap()
+                        .files_dir
+                        .clone();
+                    let mut base = PathBuf::from(&files_dir);
                     base.push(filename.as_str());
                     create_message.add_file(base.as_path());
                     let _ = send_to.send_message(&http, |_| &mut create_message).await?;
@@ -48,19 +57,36 @@ impl Task {
             }
             Task::UpdateAppearance {
                 new_name,
-                new_icon_url,
-                ..
+                new_icon_filename,
             } => {
-                let context_data = data.read().await;
-                let guild_id = context_data.get::<ConfigKey>().unwrap().guild_id;
+                let (guild_id, files_dir) = {
+                    let context = data.read().await;
+                    let config = context.get::<ConfigKey>().unwrap();
+                    (config.guild_id, config.files_dir.clone())
+                };
                 http.edit_nickname(guild_id.into(), Some(new_name.as_str()))
                     .await?;
+                let avatar_b64 = read_image(format!(
+                    "{}/{}",
+                    files_dir.trim_end_matches('/'),
+                    new_icon_filename
+                ))?;
                 http.get_current_user()
                     .await?
-                    .edit(&http, |user| user.avatar(Some(new_icon_url.as_str())))
+                    .edit(&http, |user| user.avatar(Some(&avatar_b64)))
                     .await?;
             }
         }
         Ok(())
+    }
+}
+
+impl Default for Task {
+    fn default() -> Self {
+        Task::SendMessage {
+            send_to: 0.into(),
+            message: MessageType::Plain { content: "".into() },
+            upload_file: None,
+        }
     }
 }
